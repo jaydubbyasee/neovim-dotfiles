38e75a767cb64d161087e5b5c34741c432b879e0
Jonathan Chuong <jonchuong@gmail.com>
2026-04-05 12:04:53 -0700

    Remove redundant blink install

--- nvim/lua/core/pack.lua
+++ nvim/lua/core/pack.lua
@@ -3,6 +3,5 @@ vim.pack.add({
   { src = "https://github.com/williamboman/mason.nvim.git" },
   { src = "https://github.com/williamboman/mason-lspconfig.nvim.git" },
   { src = "https://github.com/neovim/nvim-lspconfig.git" },
-  { src = "https://github.com/Saghen/blink.cmp.git" },
 })
 
